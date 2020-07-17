f8d820c260d5c4ae5105ec0129d6d174c51cf2f8
bessam-haj-salem <bessam-haj-salem@gmail.com>
2020-07-17 18:00:51 +0100

    ok

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,14 +4,14 @@ import { RmComponent } from './rm/rm.component';
 import { RsComponent } from './rs/rs.component';
 import { MainComponent } from './main/main.component';
 import { MachinesComponent } from './rm/machines/machines.component';
-import {EquipmentsComponent} from './rm/equipments/equipments.component'
-import {StockComponent} from './rm/stock/stock.component'
-import {WorkorderComponent} from './rm/workorder/workorder.component'
-import {CreateaccountComponent} from './rm/createaccount/createaccount.component'
-import {MachinslistComponent} from './rm/machines/machinslist/machinslist.component'
-import {AddmachinsComponent} from './rm/machines/addmachins/addmachins.component'
-import {PreventionComponent} from './rm/machines/prevention/prevention.component'
-import {CorrectionComponent} from './rm/machines/correction/correction.component'
+import { EquipmentsComponent } from './rm/equipments/equipments.component';
+import { StockComponent } from './rm/stock/stock.component';
+import { WorkorderComponent } from './rm/workorder/workorder.component';
+import { CreateaccountComponent } from './rm/createaccount/createaccount.component';
+import { MachinslistComponent } from './rm/machines/machinslist/machinslist.component';
+import { AddmachinsComponent } from './rm/machines/addmachins/addmachins.component';
+import { PreventionComponent } from './rm/machines/prevention/prevention.component';
+import { CorrectionComponent } from './rm/machines/correction/correction.component';
 const routes: Routes = [
   {
     path: 'director',
@@ -20,15 +20,14 @@ const routes: Routes = [
   { path: 'responsable', component: RsComponent },
   { path: '', component: MainComponent },
   { path: 'machines', component: MachinesComponent },
-  { path: 'listmachines', component: MachinesComponent },
+  { path: 'listmachines', component: MachinslistComponent },
   { path: 'addmachins', component: AddmachinsComponent },
-    { path: 'prevention', component: PreventionComponent },
-    { path: 'correction', component: AddmachinsComponent },
+  { path: 'prevention', component: PreventionComponent },
+  { path: 'correction', component: CorrectionComponent },
   { path: 'equipments', component: EquipmentsComponent },
   { path: 'stock', component: StockComponent },
   { path: 'workorder', component: WorkorderComponent },
   { path: 'create', component: CreateaccountComponent },
-
 ];
 
 @NgModule({
