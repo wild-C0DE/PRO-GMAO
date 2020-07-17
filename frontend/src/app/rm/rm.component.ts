4e2c543e853ef80b36ff39a172010dadc6838118
bessam-haj-salem <bessam-haj-salem@gmail.com>
2020-07-17 11:50:01 +0100

    skeleton version

---
+++ frontend/src/app/rm/rm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rm',
+  templateUrl: './rm.component.html',
+  styleUrls: ['./rm.component.css']
+})
+export class RmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
