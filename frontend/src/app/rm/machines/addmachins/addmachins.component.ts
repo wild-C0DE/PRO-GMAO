21d5af03b764dc2c90550d7346fd6d2a7dce50b1
bessam-haj-salem <bessam-haj-salem@gmail.com>
2020-07-18 15:50:54 +0100

    frontend add machin almost i must found a solution for the radio and the image

--- frontend/src/app/rm/machines/addmachins/addmachins.component.ts
+++ frontend/src/app/rm/machines/addmachins/addmachins.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MachinesService } from '../../../shared/machines.service'
+import {NgForm } from '@angular/forms'
 @Component({
   selector: 'app-addmachins',
   templateUrl: './addmachins.component.html',
@@ -7,11 +8,47 @@ import { MachinesService } from '../../../shared/machines.service'
   providers: [MachinesService]
 })
 export class AddmachinsComponent implements OnInit {
-
+showSuccessMessage: boolean;
+serverErrorMessages: string
   constructor(public machinesService : MachinesService) { }
 
   ngOnInit(): void {
 
+  }
+  onSubmit(form : NgForm){
+    this.machinesService.postData(form.value).subscribe(
+      res => {
+        this.showSuccessMessage =true
+        setTimeout(() => this.showSuccessMessage = false,3000)
+        this.resetForm(form)
+      },
+      err => {
+        
+          this.serverErrorMessages ="something went wrong. Please check your entry data"
+        
+      }
+    )
+      
+    
+  }
+  resetForm(form: NgForm) {
+    this.machinesService.selectedMachine = {
+      name: "",
+  reference: "",
+  family: "",
+  state: "",
+  brand: "",
+  supplierName: "",
+  supplierContact: "",
+  serialNumber: "",
+  dateOfPurchase: "",
+  inventory: "",
+  ISBN: "",
+  department: "",
+  image: "",
+    }
+    form.resetForm();
+    this.serverErrorMessages = "";
   }
 
 }
